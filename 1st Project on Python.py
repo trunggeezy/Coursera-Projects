c32e5c8165134b4d2c019e81a12a68829dec2c09
trunggeezy <144832964+trunggeezy@users.noreply.github.com>
2023-09-13 16:51:55 -0700

    Add files via upload

---
+++ 1st Project on Python.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Author
+# Trung Truong
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ***Objectives:***
+
+# Some of the popular languages that Data Scientists use are:
+# - Python
+# - R
+# - SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+#     1. TensorFlow
+#     2. sciKit-Learn
+#     3. Caffe
+
+# | Data Science Tools |
+# |:-------:|
+# | RStudio |
+# | Tensorflow |
+# | ApacheHadoop |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[2]:
+
+
+200/60
+
+
+# In[ ]:
+
+
+
+
